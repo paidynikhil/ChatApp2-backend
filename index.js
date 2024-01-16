796b2a90b3db1be9156178ea321ef1f53ce15b03
coder910 <85600909+coder910@users.noreply.github.com>
2024-01-16 22:38:34 +0530

    ChatApp2-Back

--- index.js
+++ index.js
@@ -36,19 +36,7 @@ const io = socket(server, {
   },
 });
 
-const __dirname1 = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/public/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "public", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+app.use(express.static(path.join(__dirname, "public"))); 
 
 global.onlineUsers = new Map();
 io.on("connection", (socket) => {
